83ff0932332b24df6d482c725a2480fc16f5ffac
Kain Kalju <kain@bolt.eu>
2021-04-10 13:04:08 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module git.internal/kainkalju/httpsanitizer
+
+go 1.16
+
+require (
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/knadh/koanf v0.15.0
+)
---
+++ main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/file"
+)
+
+// Global koanf instance. Use "." as the key path delimiter. This can be "/" or any character.
+var k = koanf.New(".")
+
+func main() {
+	var serverAddr string = ":8080"
+	var serverReadTimeout time.Duration = 10
+	var serverWriteTimeout time.Duration = 10
+	var serverIdleTimeout time.Duration = 20
+	var serverMaxHeaderBytes int = 4096
+
+	// Load YAML config.
+	cfg := file.Provider("config.yaml")
+	if err := k.Load(cfg, yaml.Parser()); err != nil {
+		log.Fatalf("error loading config: %v", err)
+	}
+	// Overwrite default settings with YAML config
+	if k.Exists("server.addr") {
+		serverAddr = k.String("server.addr")
+	}
+	if k.Exists("server.addr") {
+		serverReadTimeout = time.Duration(k.Int("server.readTimeout"))
+	}
+	if k.Exists("server.addr") {
+		serverWriteTimeout = time.Duration(k.Int("server.writeTimeout"))
+	}
+	if k.Exists("server.addr") {
+		serverIdleTimeout = time.Duration(k.Int("server.idleTimeout"))
+	}
+	if k.Exists("server.addr") {
+		serverMaxHeaderBytes = k.Int("server.maxHeaderBytes")
+	}
+	// Watch the file and get a callback on change. The callback can do whatever,
+	// like re-load the configuration.
+	// File provider always returns a nil `event`.
+	cfg.Watch(func(event interface{}, err error) {
+		if err != nil {
+			log.Printf("watch error: %v", err)
+			return
+		}
+
+		log.Println("config change detected. Reloading ...")
+		k.Load(cfg, yaml.Parser())
+		k.Print()
+	})
+
+	router := httprouter.New()
+	origin, _ := url.Parse("http://localhost:9000/")
+	path := "/*catchall"
+
+	reverseProxy := httputil.NewSingleHostReverseProxy(origin)
+
+	reverseProxy.Director = func(req *http.Request) {
+		req.Header.Add("X-Forwarded-Host", req.Host)
+		req.Header.Add("X-Origin-Host", origin.Host)
+		// adding CSP header here
+		// try to remove also UNKNOWN headers
+		req.URL.Scheme = origin.Scheme
+		req.URL.Host = origin.Host
+		//req.URL.RawQuery = "foo=bar&1=2" // get query_string
+
+		data := url.Values{}
+		req.ParseForm()
+		for k, vv := range req.PostForm {
+			for _, v := range vv {
+				if k == "test" {
+					v = "censored"
+				}
+				data.Set(k, v)
+			}
+		}
+		newBody := data.Encode()
+		req.Body = ioutil.NopCloser(bytes.NewBuffer([]byte(newBody)))
+		req.ContentLength = int64(len(newBody))
+
+		// wildcardIndex := strings.IndexAny(path, "*")
+		// proxyPath := singleJoiningSlash(origin.Path, req.URL.Path[wildcardIndex:])
+		// if strings.HasSuffix(proxyPath, "/") && len(proxyPath) > 1 {
+		// 	proxyPath = proxyPath[:len(proxyPath)-1]
+		// }
+		// req.URL.Path = proxyPath
+	}
+
+	router.Handle("HEAD", path, func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		startTime := time.Now()
+		reverseProxy.ServeHTTP(w, r)
+		log.Printf("from: %s %s %s%s duration: %s\n", r.RemoteAddr, r.Method, r.Host, r.RequestURI, time.Since(startTime))
+	})
+	router.Handle("GET", path, func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		startTime := time.Now()
+		reverseProxy.ServeHTTP(w, r)
+		log.Printf("from: %s %s %s%s duration: %s\n", r.RemoteAddr, r.Method, r.Host, r.RequestURI, time.Since(startTime))
+	})
+	router.Handle("POST", path, func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		startTime := time.Now()
+		reverseProxy.ServeHTTP(w, r)
+		log.Printf("from: %s %s %s%s duration: %s\n", r.RemoteAddr, r.Method, r.Host, r.RequestURI, time.Since(startTime))
+	})
+
+	server := &http.Server{
+		Addr:           serverAddr,
+		Handler:        router,
+		ReadTimeout:    serverReadTimeout * time.Second,
+		WriteTimeout:   serverWriteTimeout * time.Second,
+		IdleTimeout:    serverIdleTimeout * time.Second,
+		MaxHeaderBytes: serverMaxHeaderBytes,
+	}
+	log.Println("Starting the httpsanitizer reverse proxy server")
+	log.Fatal(server.ListenAndServe())
+}
